9d1ad05c37e58bfe2f5bad9c82eb24f7b931bafb
Chirag Mistry <cdmistry97@gmail.com>
2024-08-30 12:45:13 +0530

    Drawer Form Component Added
    Props: {
          title,
          description,
          children,
          submitLabel = "Submit",
          closeLabel = "Cancel",
          hideClose = false,
          drawerHeaderClass = "",
          size = "20%",
          direction = "right",
          onSubmit,
        }
    
    RefMethods = {
      openDrawer: () => void;
      closeDrawer: () => void;
    }
    
    Example Usage
    import React, { useEffect } from "react";
    import DrawerForm from "@/components/form/DrawerForm";
    
    import { useFormik } from "formik";
    import { Col, Grid } from "@/components/utility";
    import { Label } from "@/components/ui/label";
    import { Input } from "@/components/ui/input";
    
    interface FormValues {
      name: string;
      email: string;
      password: string;
    }
    const Index: React.FC = () => {
      const initialValues: FormValues = {
        name: "",
        email: "",
        password: "",
      };
      const formik = useFormik({
        initialValues: initialValues,
        onSubmit: (values) => {
          alert(JSON.stringify(values, null, 2));
        },
      });
    
      useEffect(() => {});
    
      return (
        <DrawerForm
          title="Create User"
          onSubmit={formik.handleSubmit}
          direction="right"
        >
          <form onSubmit={formik.handleSubmit}>
            <Grid className="p-4 pb-0">
              <Col>
                <Label htmlFor="name">Name</Label>
                <Input
                  id="name"
                  name="name"
                  type="text"
                  onChange={formik.handleChange}
                  value={formik.values.name}
                />
              </Col>
              <Col>
                <Label htmlFor="email">E-Mail</Label>
                <Input
                  id="email"
                  name="email"
                  type="email"
                  onChange={formik.handleChange}
                  value={formik.values.email}
                />
              </Col>
              <Col>
                <Label htmlFor="password">Password</Label>
                <Input
                  id="password"
                  name="password"
                  type="password"
                  onChange={formik.handleChange}
                  value={formik.values.password}
                />
              </Col>
            </Grid>
          </form>
        </DrawerForm>
      );
    };
    
    export default Index;

---
+++ src/components/form/DrawerForm.tsx
@@ -0,0 +1,126 @@
+import React, {
+  forwardRef,
+  ReactNode,
+  useEffect,
+  useImperativeHandle,
+  useState,
+} from "react";
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+} from "@/components/ui/drawer";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+
+interface IProp {
+  title?: ReactNode;
+  description?: string;
+  children: ReactNode;
+  submitLabel?: string | ReactNode;
+  closeLabel?: string | ReactNode;
+  hideClose?: boolean;
+  drawerHeaderClass?: string;
+  size?: string;
+  direction?: "right" | "left" | "bottom" | "top";
+  onSubmit: () => void;
+}
+
+export interface IndexRef {
+  openDrawer: () => void;
+  closeDrawer: () => void;
+}
+
+const Index = forwardRef<IndexRef, IProp>(
+  (
+    {
+      title,
+      description,
+      children,
+      submitLabel = "Submit",
+      closeLabel = "Cancel",
+      hideClose = false,
+      drawerHeaderClass = "",
+      size = "20%",
+      direction = "right",
+      onSubmit,
+    },
+    ref
+  ) => {
+    const navigate = useNavigate();
+
+    const [open, setOpen] = useState(true);
+
+    useImperativeHandle(ref, () => ({
+      openDrawer: () => setOpen(true),
+      closeDrawer: () => setOpen(false),
+    }));
+
+    useEffect(() => {
+      if (!open) {
+        setTimeout(() => {
+          navigate(-1);
+        }, 300);
+      }
+    }, [navigate, open]);
+
+    let directionClass: string = "";
+    switch (direction) {
+      case "right":
+        directionClass = `h-screen top-0 right-0 left-auto w-[${size}]`;
+        break;
+
+      case "left":
+        directionClass = `h-screen top-0 left-0 right-auto w-[${size}]`;
+        break;
+
+      case "top":
+        directionClass = `w-full top-0 bottom-auto h-[${size}]`;
+        break;
+
+      case "bottom":
+        directionClass = `w-full bottom-0 top-auto h-[${size}]`;
+        break;
+
+      default:
+        break;
+    }
+
+    return (
+      <Drawer
+        open={open}
+        direction={direction}
+        onOpenChange={() => setOpen(false)}
+      >
+        <DrawerContent className={`${directionClass} mt-0 rounded-none`}>
+          <DrawerHeader className={`${drawerHeaderClass}`}>
+            {title && <DrawerTitle>{title}</DrawerTitle>}
+            {description && (
+              <DrawerDescription>{description}</DrawerDescription>
+            )}
+          </DrawerHeader>
+          {children}
+          <DrawerFooter className={""}>
+            <Button type="button" onClick={onSubmit}>
+              {submitLabel}
+            </Button>
+            {!hideClose && (
+              <DrawerClose asChild>
+                <Button variant="outline" color="primary">
+                  {closeLabel}
+                </Button>
+              </DrawerClose>
+            )}
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    );
+  }
+);
+
+export default Index;
--- src/pages/Users/form.tsx
+++ src/pages/Users/form.tsx
@@ -1,80 +1,72 @@
-import React, {useEffect, useState} from "react";
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-} from "@/components/ui/drawer";
-import { Button } from "@/components/ui/button";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import DrawerForm from "@/components/form/DrawerForm";
+
+import { useFormik } from "formik";
+import { Col, Grid } from "@/components/utility";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
+interface FormValues {
+  name: string;
+  email: string;
+  password: string;
+}
 const Index: React.FC = () => {
-  const [goal, setGoal] = React.useState(350);
-  const navigate = useNavigate();
-  const [open, setOpen] = useState(true)
+  const initialValues: FormValues = {
+    name: "",
+    email: "",
+    password: "",
+  };
+  const formik = useFormik({
+    initialValues: initialValues,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
 
-  function onClick(adjustment: number) {
-    setGoal(Math.max(200, Math.min(400, goal + adjustment)));
-  }
-
-  useEffect(() => {
-    if (!open) {
-      setTimeout(() => {
-        navigate(-1);
-      }, 300)
-    }
-  }, [open]);
+  useEffect(() => {});
 
   return (
-    <Drawer open={open} direction="right" onOpenChange={() => setOpen(false)}>
-      <DrawerContent className="h-screen top-0 right-0 left-auto mt-0 w-[80%] rounded-none">
-        <div>
-          <DrawerHeader className={""}>
-            <DrawerTitle>Move Goal</DrawerTitle>
-            <DrawerDescription>Set your daily activity goal.</DrawerDescription>
-          </DrawerHeader>
-          <div className="p-4 pb-0">
-            <div className="flex items-center justify-center space-x-2">
-              <Button
-                variant="default"
-                color="primary"
-                className="h-8 w-8 shrink-0 rounded-full"
-                onClick={() => onClick(-10)}
-              >
-                -<span className="sr-only">Decrease</span>
-              </Button>
-              <div className="flex-1 text-center">
-                <div className="text-7xl font-bold tracking-tighter">
-                  {goal}
-                </div>
-                <div className="text-[0.70rem] uppercase text-muted-foreground">
-                  Calories/day
-                </div>
-              </div>
-              <Button
-                variant="default"
-                color="primary"
-                className="h-8 w-8 shrink-0 rounded-full"
-                onClick={() => onClick(10)}
-              >
-                +<span className="sr-only">Increase</span>
-              </Button>
-            </div>
-            <div className="mt-3 h-[120px]"></div>
-          </div>
-          <DrawerFooter className={""}>
-            <Button>Submit</Button>
-            <DrawerClose asChild>
-              <Button variant="default" color="primary">
-                Cancel
-              </Button>
-            </DrawerClose>
-          </DrawerFooter>
-        </div>
-      </DrawerContent>
-    </Drawer>
+    <DrawerForm
+      title="Create User"
+      onSubmit={formik.handleSubmit}
+      direction="right"
+    >
+      <form onSubmit={formik.handleSubmit}>
+        <Grid className="p-4 pb-0">
+          <Col>
+            <Label htmlFor="name">Name</Label>
+            <Input
+              id="name"
+              name="name"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.name}
+            />
+          </Col>
+          <Col>
+            <Label htmlFor="email">E-Mail</Label>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+            />
+          </Col>
+          <Col>
+            <Label htmlFor="password">Password</Label>
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+            />
+          </Col>
+        </Grid>
+      </form>
+    </DrawerForm>
   );
 };
 
