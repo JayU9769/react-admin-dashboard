e2050a87154bcee8dbb1762515c23fa24a911b0a
Sunny <sunny@Sunnys-MacBook-Pro.local>
2024-09-18 16:16:02 +0530

    - Error handler middleware added

--- src/store/middlewares/handleError.ts
+++ src/store/middlewares/handleError.ts
@@ -1,24 +1,24 @@
 import {Middleware} from "@reduxjs/toolkit";
 import {setCustomError} from "@/store/root/slice.ts";
 
-const index: Middleware = ({ dispatch }) => {
-    return next => (action: any) => {
-        if (action.type.includes("rejected") && action.error) {
-            const { pathname } = window.location
-            if (pathname !== "/" && !pathname.includes('login')) {
-                if (action.payload.status === 401) {
-                    window.location.href = "/";
-                } else {
-                    dispatch(setCustomError({
-                        message: "Oops, it looks like Something went wrong.",
-                        statusCode: action.payload.status,
-                    }))
-                }
-            }
+const index: Middleware = ({dispatch}) => {
+  return next => (action: any) => {
+    if (action.type.includes("rejected") && action.error) {
+      const {pathname} = window.location
+      if (pathname !== "/" && !pathname.includes('login')) {
+        if (action.payload.status === 401) {
+          window.location.href = "/";
+        } else {
+          dispatch(setCustomError({
+            message: "Oops, it looks like Something went wrong.",
+            statusCode: action.payload.status,
+          }))
         }
-        // Capture the current state (if you want state before the action is dispatched)
-        return next(action);
-    };
+      }
+    }
+    // Capture the current state (if you want state before the action is dispatched)
+    return next(action);
+  };
 };
 
 
