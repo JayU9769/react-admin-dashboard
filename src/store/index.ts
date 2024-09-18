40bd836aad51be7bca9863affcf130122fd405cd
Jay Parmar <parmarjaydeep9769@gmail.com>
2024-09-18 11:16:39 +0530

    - Local commit

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
+import {configureStore} from "@reduxjs/toolkit";
 import rootReducer from "@/store/root/slice";
 import userReducer from "@/store/user/slice";
 import roleReducer from "@/store/roles/slice";
 import adminReducer from "@/store/admin/slice";
-import { ThunkAction } from "redux-thunk";
-import { Action } from "redux";
-import { rootApi } from "@/store/root/api.ts";
-import { userApi } from "@/store/user/api.ts";
-import { roleApi } from "@/store/roles/api.ts";
-import { adminApi } from "@/store/admin/api.ts";
+import {ThunkAction} from "redux-thunk";
+import {Action} from "redux";
+import {rootApi} from "@/store/root/api.ts";
+import {userApi} from "@/store/user/api.ts";
+import {roleApi} from "@/store/roles/api.ts";
+import {adminApi} from "@/store/admin/api.ts";
+
+
+// Custom Sentry middleware to log actions and state changes
+const sentryLogMiddleware = ({ getState }) => {
+  return next => action => {
+    console.log(action);
+    // Capture the current state (if you want state before the action is dispatched)
+    return next(action);
+  };
+};
+
 
 // Use `configureStore` function to create the store:
 const store = configureStore({
@@ -30,7 +41,8 @@ const store = configureStore({
       .concat(rootApi.middleware)
       .concat(userApi.middleware)
       .concat(roleApi.middleware)
-      .concat(adminApi.middleware),
+      .concat(adminApi.middleware)
+      .concat(sentryLogMiddleware),
 });
 
 // Define the `RootState` as the return type:
